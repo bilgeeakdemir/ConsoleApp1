a2ab951135530d0dbe6b9124c1822a74b3e0f1c3
eakde <eakde@MAYIS>
2023-10-12 13:40:01 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.Write("name please:\t\t  ");
+            name = Console.ReadLine();
+            string age;
+            Console.Write("age please: \t\t ");
+            age = Console.ReadLine();
+
+            string job;
+            Console.Write("job please:\t\t  ");
+            job = Console.ReadLine();
+
+            string cat;
+            Console.Write("cat please:\t\t ");
+            cat = Console.ReadLine();
+
+            Console.WriteLine(name  + age + job + cat);
+
+
+
+            float e = 25.4f;
+            Console.WriteLine(e);
+
+            Console.ReadKey();
+        }
+    }
+}
